2e33ad8e45c0179d0536508ee1e1ad4074ced70d
nazaninbinesh <nazanin_binesh@yahoo.com>
2021-03-09 11:32:47 -0800

    adding list to food track activity

--- app/src/main/java/com/example/nextdoordocapp/PatientFoodTrackPg1.java
+++ app/src/main/java/com/example/nextdoordocapp/PatientFoodTrackPg1.java
@@ -2,13 +2,25 @@ package com.example.nextdoordocapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PatientFoodTrackPg1 extends AppCompatActivity {
+    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_food_track_pg1);
+
+        addButton=findViewById(R.id.btnPatAddFoodToBreakfast);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PatientFoodTrackPg1.this,PatientFoodSearch.class));
+            }
+        });
     }
 }
