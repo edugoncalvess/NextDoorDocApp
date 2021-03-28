bbed4e4c248b110d322840c716a237e04a168714
AnuKumaravelan <74986860+AnuKumaravelan@users.noreply.github.com>
2021-03-28 00:55:30 -0700

    Doctor Registration Module

--- app/src/main/java/com/example/nextdoordocapp/AdmDocInformation.java
+++ app/src/main/java/com/example/nextdoordocapp/AdmDocInformation.java
@@ -3,6 +3,10 @@ package com.example.nextdoordocapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AdmDocInformation extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class AdmDocInformation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adm_doc_registration);
+DatabaseHelper databaseHelper= new DatabaseHelper(this);
+        EditText txtDocFName=findViewById(R.id.txtRgstDocFirstName);
+        EditText txtDocLName=findViewById(R.id.txtRgstDocFamilyName);
+        EditText txtDocEmail=findViewById(R.id.txtRgstDocEmail);
+        EditText txtDocPassword= findViewById(R.id.txtRgstDocPassword);
+        EditText txtDocPhone= findViewById(R.id.txtRgstDocPhone);
+        EditText txtDocAddress= findViewById(R.id.txtRgstDocAddress);
+        EditText txtDocCity= findViewById(R.id.txtRgstDocCity);
+        EditText txtDocPCode= findViewById(R.id.txtRgstDocPCode);
+
+        Button createDocProf=findViewById(R.id.btnRgstDocCreate);
+
+        createDocProf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String dEmail,dFName,dLName, dPassword, dPCode, dPhone, dAddress,dCity;
+                dEmail=txtDocEmail.getText().toString();
+                dFName=txtDocFName.getText().toString();
+                dLName=txtDocLName.getText().toString();
+                dPassword=txtDocPassword.getText().toString();
+                dPCode=txtDocPCode.getText().toString();
+                dPhone=txtDocPhone.getText().toString();
+                dAddress=txtDocAddress.getText().toString();
+                dCity=txtDocCity.getText().toString();
+
+                databaseHelper.addDoctorRecords(dEmail,dFName,dLName,
+                        dPassword,dPCode,dPhone,dAddress,dCity);
+                Toast.makeText(AdmDocInformation.this, "Profile Created", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
